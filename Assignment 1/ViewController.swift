7f23a7873cbdc1512face0f5c05a58f63ef12560
Jannet Kim <jannetkim@jannets-macbook-pro.local>
2015-11-01 21:14:48 -0800

    Initial Commit

---
+++ Assignment 1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Assignment 1
+//
+//  Created by Jannet Kim on 11/1/15.
+//  Copyright © 2015 Jannet Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
